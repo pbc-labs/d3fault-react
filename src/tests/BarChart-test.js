66fba0241b7e5eaa1fd31789fcdc063c74a7679d
Preethi <iam.preethi.k@gmail.com>
2015-12-30 15:15:50 -0800

    Add unit tests to test customizable prop functionality

--- src/tests/BarChart-test.js
+++ src/tests/BarChart-test.js
@@ -121,6 +121,119 @@ describe('BarChart component', function () {
   });
 });
 
+describe('BarChart component - setting customized props', function () {
+
+  beforeEach(() => {
+  });
+
+  it('should overwrite default prop type for title - vdom', () => {
+    this.tree = sd.shallowRender($(BarChart, { title: 'New title'}));
+    this.vdom = this.tree.getRenderOutput();
+    this.mounted = this.tree.getMountedInstance();
+    expect(this.vdom.props.children.props.title).to.equal('New title');
+  });
+
+  it('should overwrite default prop type for title - mounted', () => {
+    this.tree = sd.shallowRender($(BarChart, { title: 'New title'}));
+    this.vdom = this.tree.getRenderOutput();
+    this.mounted = this.tree.getMountedInstance();
+    expect(this.mounted.props.title).to.equal('New title');
+  });
+
+  it('should overwrite default prop type for width - vdom', () => {
+    this.tree = sd.shallowRender($(BarChart, { width: 400 }));
+    this.vdom = this.tree.getRenderOutput();
+    this.mounted = this.tree.getMountedInstance();
+    expect(this.vdom.props.children.props.width).to.equal(400);
+  });
+
+  it('should overwrite default prop type for width - mounted', () => {
+    this.tree = sd.shallowRender($(BarChart, { width: 400 }));
+    this.vdom = this.tree.getRenderOutput();
+    this.mounted = this.tree.getMountedInstance();
+    expect(this.mounted.props.width).to.equal(400);
+  });
+
+  it('should overwrite default prop type for height - vdom', () => {
+    this.tree = sd.shallowRender($(BarChart, { height: 500 }));
+    this.vdom = this.tree.getRenderOutput();
+    this.mounted = this.tree.getMountedInstance();
+    expect(this.vdom.props.children.props.height).to.equal(500);
+  });
+
+  it('should overwrite default prop type for height - mounted', () => {
+    this.tree = sd.shallowRender($(BarChart, { height: 500 }));
+    this.vdom = this.tree.getRenderOutput();
+    this.mounted = this.tree.getMountedInstance();
+    expect(this.mounted.props.height).to.equal(500);
+  });
+
+  it('should overwrite default prop type for margins - vdom', () => {
+    this.tree = sd.shallowRender($(BarChart, { margins: { top: 60, right: 60, bottom: 60, left: 60 } }));
+    this.vdom = this.tree.getRenderOutput();
+    this.mounted = this.tree.getMountedInstance();
+    expect(this.vdom.props.children.props.margins.top).to.equal(60);
+    expect(this.vdom.props.children.props.margins.right).to.equal(60);
+    expect(this.vdom.props.children.props.margins.bottom).to.equal(60);
+    expect(this.vdom.props.children.props.margins.left).to.equal(60);
+  });
+
+  it('should overwrite default prop type for margins - mounted', () => {
+    this.tree = sd.shallowRender($(BarChart, { margins: { top: 60, right: 60, bottom: 60, left: 60 } }));
+    this.vdom = this.tree.getRenderOutput();
+    this.mounted = this.tree.getMountedInstance();
+    expect(this.mounted.props.margins.top).to.equal(60);
+    expect(this.mounted.props.margins.right).to.equal(60);
+    expect(this.mounted.props.margins.bottom).to.equal(60);
+    expect(this.mounted.props.margins.left).to.equal(60);
+  });
+
+  it('should overwrite default prop type for fontStyle - vdom', () => {
+    this.tree = sd.shallowRender($(BarChart, { fontStyle: 'Comic Sans' }));
+    this.vdom = this.tree.getRenderOutput();
+    this.mounted = this.tree.getMountedInstance();
+    expect(this.vdom.props.children.props.fontStyle).to.equal('Comic Sans');
+  });
+
+  it('should overwrite default prop type for fontStyle - mounted', () => {
+    this.tree = sd.shallowRender($(BarChart, { fontStyle: 'Comic Sans' }));
+    this.vdom = this.tree.getRenderOutput();
+    this.mounted = this.tree.getMountedInstance();
+    expect(this.mounted.props.fontStyle).to.equal('Comic Sans');
+  });
+
+  it('should overwrite default prop type for fontSize - vdom', () => {
+    this.tree = sd.shallowRender($(BarChart, { fontSize: 22 }));
+    this.vdom = this.tree.getRenderOutput();
+    this.mounted = this.tree.getMountedInstance();
+    expect(this.vdom.props.children.props.fontSize).to.equal(22);
+  });
+
+  it('should overwrite default prop type for fontSize - mounted', () => {
+    this.tree = sd.shallowRender($(BarChart, { fontSize: 22 }));
+    this.vdom = this.tree.getRenderOutput();
+    this.mounted = this.tree.getMountedInstance();
+    expect(this.mounted.props.fontSize).to.equal(22);
+  });
+
+  it('should overwrite default prop type for titleFontSize - vdom', () => {
+    this.tree = sd.shallowRender($(BarChart, { titleFontSize: 24 }));
+    this.vdom = this.tree.getRenderOutput();
+    this.mounted = this.tree.getMountedInstance();
+    expect(this.vdom.props.children.props.titleFontSize).to.equal(24);
+  });
+
+  it('should overwrite default prop type for fontSize - mounted', () => {
+    this.tree = sd.shallowRender($(BarChart, { titleFontSize: 24 }));
+    this.vdom = this.tree.getRenderOutput();
+    this.mounted = this.tree.getMountedInstance();
+    expect(this.mounted.props.titleFontSize).to.equal(24);
+  });
+
+});
+
+
+
 describe('BarChartLeft component', function () {
 
   beforeEach(() => {
@@ -151,29 +264,29 @@ describe('BarChartLeft component', function () {
     expect(this.vdom.props.children.props.height).to.equal(400);
   });
 
-  it("mounts the BarChart with height prop set to default height", () => {
+  it("mounts the BarChartLeft with height prop set to default height", () => {
     expect(this.mounted.props).to.have.property('height');
     expect(this.mounted.props.height).to.equal(400);
   });
 
-  it("renders the BarChart with title prop set to default title", () => {
+  it("renders the BarChartLeft with title prop set to default title", () => {
     expect(this.vdom.props.children.props).to.have.property('title');
     expect(this.vdom.props.children.props.title).to.equal('Default Title');
   });
 
-  it("mounts the BarChart with title prop set to default title", () => {
+  it("mounts the BarChartLeft with title prop set to default title", () => {
     expect(this.mounted.props).to.have.property('title');
     expect(this.mounted.props.title).to.equal('Default Title');
   });
 
-  it("renders the BarChart with margins prop set to default margins", () => {
+  it("renders the BarChartLeft with margins prop set to default margins", () => {
     expect(this.vdom.props.children.props).to.have.property('margins');
     expect(this.vdom.props.children.props.margins.top).to.equal(30);    expect(this.vdom.props.children.props.margins.bottom).to.equal(50);
     expect(this.vdom.props.children.props.margins.left).to.equal(55);
     expect(this.vdom.props.children.props.margins.right).to.equal(30);
   });
 
-  it("mounts the BarChart with margins prop set to default margins", () => {
+  it("mounts the BarChartLeft with margins prop set to default margins", () => {
     expect(this.mounted.props).to.have.property('margins');
     expect(this.mounted.props.margins.top).to.equal(30);
     expect(this.mounted.props.margins.bottom).to.equal(50);
@@ -181,47 +294,47 @@ describe('BarChartLeft component', function () {
     expect(this.mounted.props.margins.right).to.equal(30);
   });
 
-  it("renders the BarChart with fontSize prop set to default fontSize", () => {
+  it("renders the BarChartLeft with fontSize prop set to default fontSize", () => {
     expect(this.vdom.props.children.props).to.have.property('fontSize');
     expect(this.vdom.props.children.props.fontSize).to.equal(14);
   });
 
-  it("mounts the BarChart with fontSize prop set to default fontSize", () => {
+  it("mounts the BarChartLeft with fontSize prop set to default fontSize", () => {
     expect(this.mounted.props).to.have.property('fontSize');
     expect(this.mounted.props.fontSize).to.equal(14);
   });
 
-  it("renders the BarChart with fontStyle prop set to default fontStyle", () => {
+  it("renders the BarChartLeft with fontStyle prop set to default fontStyle", () => {
     expect(this.vdom.props.children.props).to.have.property('fontStyle');
     expect(this.vdom.props.children.props.fontStyle).to.equal('Sans Serif');
   });
 
-  it("mounts the BarChart with fontStyle prop set to default fontStyle", () => {
+  it("mounts the BarChartLeft with fontStyle prop set to default fontStyle", () => {
     expect(this.mounted.props).to.have.property('fontStyle');
     expect(this.mounted.props.fontStyle).to.equal('Sans Serif');
   });
 
-  it("renders the BarChart with titleFontSize prop set to default titleFontSize", () => {
+  it("renders the BarChartLeft with titleFontSize prop set to default titleFontSize", () => {
     expect(this.vdom.props.children.props).to.have.property('titleFontSize');
     expect(this.vdom.props.children.props.titleFontSize).to.equal(18);
   });
 
-  it("mounts the BarChart with titleFontSize prop set to default titleFontSize", () => {
+  it("mounts the BarChartLeft with titleFontSize prop set to default titleFontSize", () => {
     expect(this.mounted.props).to.have.property('titleFontSize');
     expect(this.mounted.props.titleFontSize).to.equal(18);
   });
 
-  it("renders the BarChart with mainClassName prop set to 'd3fault-chart'", () => {
+  it("renders the BarChartLeft with mainClassName prop set to 'd3fault-chart'", () => {
     expect(this.vdom.props.children.props).to.have.property('mainClassName');
     expect(this.vdom.props.children.props.mainClassName).to.equal('d3fault-chart');
   });
 
-  it("renders the BarChart with titleClassName prop set to 'd3fault-title'", () => {
+  it("renders the BarChartLeft with titleClassName prop set to 'd3fault-title'", () => {
     expect(this.vdom.props.children.props).to.have.property('titleClassName');
     expect(this.vdom.props.children.props.titleClassName).to.equal('d3fault-title');
   });
 
-  it("renders the BarChart with svgClassName prop set to 'd3fault-svg'", () => {
+  it("renders the BarChartLeft with svgClassName prop set to 'd3fault-svg'", () => {
     expect(this.vdom.props.children.props).to.have.property('svgClassName');
     expect(this.vdom.props.children.props.svgClassName).to.equal('d3fault-svg');
   });
